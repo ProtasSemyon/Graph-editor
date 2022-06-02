708670ce7c5d2eef234f62715270f8e7d6d36800
Semen <sem1235463782@gmail.com>
2022-06-02 18:06:38 +0300

    Add files

---
+++ Entities.py
@@ -0,0 +1,38 @@
+colors = ["red", "green", "blue", "black", "yellow", "gray"]
+shapes = ["dot", "square", "star", "diamond", "triangle"]
+
+class Node:
+    def __init__(self, label: str, color: str = "black", shape: str = "dot"):
+        try:
+            assert label != ""
+        except AssertionError:
+            print("Node label can't be empty")
+            raise SystemExit
+        self.label = label
+        
+        try:
+            assert color in colors
+        except AssertionError:
+            print("Wrong color")
+            raise SystemExit
+        self.color = color
+
+        try:
+            assert shape in shapes
+        except AssertionError:
+            print("Wrong shape")
+            raise SystemExit
+        self.shape = shape
+
+class Edge:
+    def __init__(self, first: Node, second: Node, isOriented: bool = False, color: str = "black", label: str = ""):
+        self.label = label
+        self.first = first
+        self.second = second
+        try:
+            assert color in colors
+        except AssertionError:
+            print("Wrong color")
+            raise SystemExit
+        self.color = color
+        self.isOriented = isOriented
---
+++ main.py
@@ -0,0 +1,45 @@
+import click
+from Graph import Graph
+from pyvis.network import Network
+import os
+
+@click.group()
+def run() -> None:
+    pass
+
+@run.command('show')
+def show():
+    os.system(".\current.html")
+
+
+def output(graph):
+    net = Network("650px", "1500px", heading = graph.name)
+    for node in graph.nodes:
+        net.add_node(node.label, label = node.label, color = node.color, shape = node.shape)
+    for edge in graph.edges:
+        net.directed = edge.isOriented
+        net.add_edge(edge.first, edge.second, color = edge.color, label = edge.label)
+    net.save_graph("current.html")
+    net.save_graph(f"{graph.name}.html")
+
+
+
+
+if __name__ == "__main__":
+    with open("current.txt", "r") as nameInfo:
+        name = nameInfo.read()
+    g = Graph(name)
+    g.load("current.html")
+    # g.add_node("1", "red", "dot")
+    # g.add_node("2", "red", "dot")
+    # g.add_edge("1", "2", True, "black", "23")
+    output(g)
+    #run()
+
+
+
+
+
+
+
+
